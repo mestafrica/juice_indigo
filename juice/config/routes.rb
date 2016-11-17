d801044c8530f8f22de54438c81706033ce954f6
ZolileN <zolile@mlkcomputer.co.za>
2016-11-17 16:56:23 +0000

    Upload a file

--- juice/config/routes.rb
+++ juice/config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+root "resumes#index"
+resources :resumes, only: [:index, :new, :create, :destroy]
+
+   
+
+  get 'resumes/index'
+
+  get 'resumes/new'
+
+  get 'resumes/create'
+
+  get 'resumes/destroy'
+
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
